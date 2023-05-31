c3cfe37dcc38e5b5c4c3a2350e3d57e08eb45deb
Jose <jccb.15@gmail.com>
2023-05-31 13:50:30 +0900

    implemented main method to retrieve weather from a given city

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,25 @@
-void main(List<String> args) {
-  print(args);
+import 'dart:io';
+
+import 'weather_api.dart';
+
+void main(List<String> args) async {
+  if (args.length != 1) {
+    print("Usage: dart bin/main.dart <city>");
+    return;
+  }
+
+  final city = args.first;
+  final api = WeatherApi();
+
+  try {
+    final (lat: lat, long: long) = await api.fetchLatLong(city);
+    final weather = await api.fetchWeather(lat, long);
+    print(weather);
+  } on WeatherApiException catch (e) {
+    print(e.message);
+  } on SocketException catch (_) {
+    print('Could not fetch data. Check your connection.');
+  } catch (e) {
+    print(e);
+  }
 }
